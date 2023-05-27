2b87ecea36560ee67b1c1b25a1506de00e4d0d14
JP <jplmarotinho22@gmail.com>
2023-05-27 16:46:32 -0300

    feat: :sparkles: knex added to dependencies, first migration created

---
+++ src/database.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line import/no-named-default
+import { default as setupKnex, Knex } from 'knex'
+
+export const config: Knex.Config = {client: 'sqlite3',
+connection: {
+  filename: './db/app.db',
+},
+useNullAsDefault: true,
+migrations: {
+  extension: 'ts',
+  directory: './db/migrations',
+}
+}
+
+export const knex = setupKnex(config)
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 /* eslint-disable prettier/prettier */
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 // http://localhost:3333/hello
 
-app.get('/hello', () => {
-  return 'Hello world'
+app.get('/hello', async() => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
 })
 
 app
